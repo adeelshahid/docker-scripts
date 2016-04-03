518924eb746c5f97f15efd46cd3da56f941d29ea
Muhammad Adeel <imadeelshahid@gmail.com>
2016-04-03 07:48:30 +0200

    install for ubuntu-15

---
+++ install/ubuntu-15.sh
@@ -0,0 +1,20 @@
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get update
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+sudo echo "/etc/apt/sources.list.d/docker.list" >> /etc/apt/sources.list.d/docker.list
+
+sudo apt-get update
+sudo apt-get purge lxc-docker
+
+sudo apt-cache policy docker-engine
+
+sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r)
+
+sudo apt-get update
+sudo apt-get install docker-engine
+
+sudo service docker start
+sudo docker run hello-world
